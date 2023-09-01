c42acb49958a50f30a7a117e731e4d9bad31aeef
Bryan España <bryanespana21550@gmail.com>
2023-09-01 13:13:20 -0600

    Segundo diseño

--- app/src/main/java/com/example/logingalery/auth/login.kt
+++ app/src/main/java/com/example/logingalery/auth/login.kt
@@ -2,6 +2,7 @@ package com.example.logingalery.auth
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -29,11 +30,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import androidx.core.content.res.FontResourcesParserCompat.FamilyResourceEntry
 import com.example.logingalery.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -50,40 +56,57 @@ fun LoginPage( modifier: Modifier = Modifier) {
     val twitter = painterResource(id = R.drawable.x_twitter)
     //Texto fuerte
     val modifierTextBold = modifier.padding(start = 20.dp)
-
+    //Fuentes
+   // val font = FontFamily(Font(R.font.lexend_mega))
     Surface (
         Modifier
             .fillMaxSize()
             .background(Color(0xFFf6f6f6)),
-        color = Color(0xFFf6f6f6)
+        color = Color(0xFF040b26)
     ) {
         Column( modifier.fillMaxWidth()) {
-            Image(painter = logo, contentDescription =null,
+            Column (
                 modifier
-                    .fillMaxWidth()
-                    .padding(top = 50.dp))
-            Text(text = "Iniciar sesión", modifier = modifierTextBold, fontSize = 25.sp)
-            OutlinedTextField(value = text, onValueChange = {},
-                modifier
-                    .fillMaxWidth()
-                    .padding(20.dp), label = {Text(text = "Correo electrónico")},
-            )
-            OutlinedTextField(value = text, onValueChange = {},
-                modifier
-                    .fillMaxWidth()
-                    .padding(20.dp), label = {Text(text = "Contraseña")})
+                    .padding(vertical = 50.dp)
+                    .align(Alignment.CenterHorizontally)) {
+                Text(text = "LOGIN TO", modifier.align(Alignment.CenterHorizontally),
+                    fontSize = 35.sp, color=Color.White, textAlign = TextAlign.Center
 
-            Divider(modifier.padding(horizontal = 20.dp))
+                )
+                Text(text = "YOUR ACCOUNT", modifier.align(Alignment.CenterHorizontally),
+                    fontSize = 35.sp, color=Color.White, textAlign = TextAlign.Center,
+                )
+            }
+            Column (
+                Modifier.background(color = Color(0xFF08215e))
+                    .padding(vertical = 20.dp)
+            ) {
+                Text(text = "Enter your login information",
+                    modifier.align(Alignment.CenterHorizontally),
+                    fontSize = 18.sp, color=Color(0xFF243156))
+                OutlinedTextField(value = text, onValueChange = {},
+                    modifier
+                        .fillMaxWidth()
+                        .padding(20.dp), label = {Text(text = "Correo electrónico")},
+                )
+                OutlinedTextField(value = text, onValueChange = {},
+                    modifier
+                        .fillMaxWidth()
+                        .padding(20.dp), label = {Text(text = "Contraseña")})
+                Button(onClick = { /*TODO*/ } ,
+                    modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)) {
+                    Text(text = "Ingresar")
+                }
+
+            }
+
             Text(text = "- o inicia sesión con -",
                 modifier
                     .align(Alignment.CenterHorizontally)
                     .padding(10.dp))
-            Button(onClick = { /*TODO*/ } ,
-                modifier
-                    .fillMaxWidth()
-                    .padding(20.dp)) {
-                Text(text = "Ingresar")
-            }
+
             Row( modifier.align(Alignment.CenterHorizontally)) {
                 Card(
                     modifier
@@ -124,7 +147,7 @@ fun LoginPage( modifier: Modifier = Modifier) {
                 modifier
                     .fillMaxHeight()
                     .align(Alignment.CenterHorizontally)) {
-                Text(text = "¿No tienes cuenta aun? Registrate ",modifier.align(Alignment.CenterHorizontally))
+                Text(text = "¿No tienes cuenta aun? Registrate ", modifier.align(Alignment.CenterHorizontally), color = Color.White)
             }
         }
     }
